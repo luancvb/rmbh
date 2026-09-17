43cccc13aa646692406c678c394d3a6ea45ac247
Luan Brasileiro <107136985+luancvb@users.noreply.github.com>
2026-09-17 20:55:14 -0300

    Add files via upload

---
+++ dados/config.js
@@ -0,0 +1,12 @@
+/* Configuração do progresso compartilhado.
+ *
+ * Cole aqui o endereço do aplicativo da Web que você implantou no Google Apps
+ * Script (ele termina em /exec). O passo a passo está no README, na seção
+ * "Progresso compartilhado".
+ *
+ * Enquanto isto ficar vazio, a página funciona normalmente, mas cada pessoa vê
+ * só o próprio progresso, salvo no navegador dela.
+ */
+window.CONFIG = {
+  apiUrl: "https://script.google.com/macros/s/AKfycbz59uLa6rxqCw3mZXoZS5E1TWbaLhijnWtsMCjT-gh_C8--Mh8QDAjxoh6ZrTfbhefF/exec"
+};
